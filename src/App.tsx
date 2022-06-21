1039c8db17d6196e17f4349fc8836e29fcf869f4
Danilo <daniloworkdev@gmail.com>
2022-06-21 12:50:24 +0100

    feat: setup React, GraphQL, Apollo and Tailwind CSS finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,37 @@
+import { gql , useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+ const GET_LESSONS_QUERY = gql`
+  query MyQuery {
+  lessons {
+    id
+    title
+  }
+}`
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
+/*   useEffect(() =>{   outra forma de fazer a requisição 
+    client.query({
+      query: GET_LESSONS_QUERY
+    }).then(response => {
+      console.log(response.data)
+    })
+  },[]) */
+
+  const { data } = useQuery<{ lessons: Lesson[]}>(GET_LESSONS_QUERY)
+
   return (
-      <h1 className="text-5xl font-bold text-violet-500">Hello World</h1>
+      <ul>
+        {data?.lessons.map(lesson => {
+          return <li key={lesson.id}>{lesson.title}</li>
+        })}
+      </ul>
   )
 }
 
