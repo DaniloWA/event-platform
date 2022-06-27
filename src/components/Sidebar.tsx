f5404796fa19da01bb28806633505f659adddc0d
Danilo Oliveira <Daniloworkdev@gmail.com>
2022-06-27 20:18:19 +0100

    feat: implementing query for lesson

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,8 +1,17 @@
 import { Lesson } from "./Lesson";
 
-export function Sidebar(){
-    return(
-        <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+const GET_LESSONS_QUERY = gql`
+  query MyQuery {
+    lessons(orderBy: publishedAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      slug
+      title
+      availableAt
+    }
+  }
+`;
+
             <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
                 Cronograma de aulas
             </span>
